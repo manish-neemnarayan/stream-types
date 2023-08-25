25861590028732475c328303179190f69a2d5257
manish-neemnarayan <manish9301kumar@gmail.com>
2023-08-25 09:45:10 +0530

    feat: using read.js can read-write big data efficiently

---
+++ readable-stream/README.md
@@ -0,0 +1,5 @@
+## This folders file read.js implements a functionality which reads big data and write it to a dest.txt file.   
+
+### Step: 1. For making a big file named test.txt you just need to run writeMany.js file in first-stream folder. It will create a file of size of 9.9gb without freezing your computer.  
+
+### Step: 2. Now you have a big file in readable-stream named as test.txt of size 9.9gb. Now run the read.js file which reads from test.txt and writes to dest.txt without freezing your pc and that too blazingly fast with only ~42mb memory consumption and ~100% (1 core cpu). 😀
--- readable-stream/read.js
+++ readable-stream/read.js
@@ -1,11 +1,24 @@
+const { copyFileSync } = require("node:fs");
 const fs = require("node:fs/promises");
 
 (async function() {
     const fileHandler = await fs.open("./test.txt", "r");
-    const stream = fileHandler.createReadStream();
+    const fileHandlerWrite = await fs.open("./dest.txt", "w");
+    const streamRead = fileHandler.createReadStream();
+    const streamWrite = fileHandlerWrite.createWriteStream();
+    console.time("read many");
+    streamRead.on("data", (chunk) => {
+        if(!streamWrite.write(chunk)) {
+            streamRead.pause();
+        }        
+    })
+    
+    streamWrite.on("drain", () => {
+        streamRead.resume();
+    })
 
-    stream.on("data", (chunk) => {
-        console.log("-------------");
-        console.log(chunk);
+    streamRead.on("end", () => {
+        fileHandler.close();
+        console.timeEnd("read many");
     })
 })()
